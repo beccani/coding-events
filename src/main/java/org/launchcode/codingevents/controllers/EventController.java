94df418625b7c77ca1baab691616e184282829fb
Becca Johnson <beccanjohnson@gmail.com>
2021-03-18 16:00:13 -0500

    -m "model binding, completed ch 13"

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -4,8 +4,10 @@ import org.launchcode.codingevents.data.EventData;
 import org.launchcode.codingevents.models.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,12 +26,19 @@ public class EventController {
     @GetMapping("create")
     public String renderCreateEventForm(Model model) {
         model.addAttribute("title", "Create Event");
+        model.addAttribute(new Event());
         return "events/create";
     }
 
     // lives at /events/create
     @PostMapping("create")
-    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+    public String processCreateEventForm(@ModelAttribute @Valid Event newEvent,
+                                         Errors errors, Model model) {
+        if(errors.hasErrors()) {
+            model.addAttribute("title", "Create Event");
+            return "events/create";
+        }
+
         EventData.add(newEvent);
         return "redirect:";
     }
@@ -51,21 +60,24 @@ public class EventController {
         return "redirect:";
     }
 
+    @GetMapping("edit")
+    public String displayEditForm(Model model, @PathVariable int eventId) {
+        //code
+        //model.addAttribute();
+        return "edit";
+    }
+
+//    @PostMapping("edit/${eventId}")
+//    public String processEditForm(int eventId, String name, String description) {
+//        //
+//    }
 
 
 
 
 }
+
+
 //
-//    @GetMapping("edit")
-//    public String displayEditForm(Model model, @PathVariable int eventId) {
-//        //code
-//        model.addAttribute();
-//        return "edit";
-//    }
-//
-//    @PostMapping("edit/${eventId}")
-//    public String processEditForm(int eventId, String name, String description) {
-//        //
-//    }
+
 
