1952a93fc19244f97a761543396ce04fd02bc296
Becca Johnson <beccanjohnson@gmail.com>
2021-03-21 18:59:57 -0500

    -m "created AbstractEntity to dry up code"

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -8,11 +8,7 @@ import javax.validation.constraints.NotBlank;
 import java.util.Objects;
 
 @Entity
-public class EventCategory {
-
-    @Id
-    @GeneratedValue
-    private int id;
+public class EventCategory extends AbstractEntity{
 
     @NotBlank
     private String name;
@@ -22,14 +18,6 @@ public class EventCategory {
     }
 
     public EventCategory() {
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getName() {
@@ -42,15 +30,4 @@ public class EventCategory {
 
     @Override
     public String toString() {return name;}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if ( o == null || getClass() != o.getClass()) return false;
-        EventCategory eventCategory = (EventCategory) o;
-        return id == eventCategory.id;
-    }
-
-    @Override
-    public int hashCode() { return Objects.hash(id); }
 }
